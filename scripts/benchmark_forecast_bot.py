53871e9beffc7d7ba5a477d3ff49202eac2774a9
Ben Wilson <wilson.benjamin1000@gmail.com>
2024-12-21 16:52:59 +0000

    Added new experimental bots

--- scripts/benchmark_forecast_bot.py
+++ scripts/benchmark_forecast_bot.py
@@ -8,6 +8,15 @@ import typeguard
 from forecasting_tools.ai_models.resource_managers.monetary_cost_manager import (
     MonetaryCostManager,
 )
+from forecasting_tools.forecasting.forecast_bots.experiments.exa_bot import (
+    ExaBot,
+)
+from forecasting_tools.forecasting.forecast_bots.experiments.q3_template_bot import (
+    Q3TemplateBot,
+)
+from forecasting_tools.forecasting.forecast_bots.experiments.q4_main_binary_bot import (
+    Q4MainBinaryBot,
+)
 from forecasting_tools.forecasting.forecast_bots.forecast_bot import (
     ForecastBot,
 )
@@ -21,9 +30,18 @@ logger = logging.getLogger(__name__)
 
 
 async def benchmark_forecast_bot() -> None:
-    questions_to_use = 2
+    questions_to_use = 120
     with MonetaryCostManager() as cost_manager:
-        bots = [TemplateBot(), TemplateBot(), TemplateBot()]
+        bots = [
+            ExaBot(),
+            Q4MainBinaryBot(),
+            Q3TemplateBot(),
+            TemplateBot(),
+            Q3TemplateBot(
+                research_reports_per_question=3,
+                predictions_per_research_report=3,
+            ),
+        ]
         bots = typeguard.check_type(bots, list[ForecastBot])
         benchmarks = await Benchmarker(
             number_of_questions_to_use=questions_to_use,
@@ -32,11 +50,12 @@ async def benchmark_forecast_bot() -> None:
         ).run_benchmark()
         for i, benchmark in enumerate(benchmarks):
             logger.info(f"Benchmark {i+1} of {len(benchmarks)}")
-            logger.info(f"- Total Cost: {cost_manager.current_usage}")
             logger.info(
                 f"- Final Score: {benchmark.average_inverse_expected_log_score}"
             )
+            logger.info(f"- Total Cost: {benchmark.total_cost}")
             logger.info(f"- Time taken: {benchmark.time_taken_in_minutes}")
+        logger.info(f"Total Cost: {cost_manager.current_usage}")
 
 
 if __name__ == "__main__":
