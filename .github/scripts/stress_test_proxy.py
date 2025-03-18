6f2809574d2a8fefc66b07fc87cb8c6c3524aaf0
Ben Wilson <wilson.benjamin1000@gmail.com>
2025-03-18 19:13:37 +0000

    Updates to proxy tests

--- .github/scripts/stress_test_proxy.py
+++ .github/scripts/stress_test_proxy.py
@@ -70,6 +70,11 @@ async def stress_test_proxy() -> None:
             logger.info(f"Number of {error_type} errors: {count}")
 
         print("Finished")
+        if number_of_errors > 0:
+            raise Exception("There were errors")
+
+        if cost_manager.current_usage > 3:
+            raise Exception("Cost is too high")
 
 
 if __name__ == "__main__":
